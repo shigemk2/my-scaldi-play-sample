e1e33fcc2c274c7740273437f91b427a7266275f
shigemk2 <i.am.shige@gmail.com>
2016-07-26 02:54:38 +0900

    Delete binding identifiedBy

--- app/modules/WebModule.scala
+++ app/modules/WebModule.scala
@@ -5,6 +5,4 @@ import service.{MessageService, OfficialMessageService}
 
 class WebModule extends Module {
   bind [MessageService] to new OfficialMessageService
-
-  binding identifiedBy "greeting.official" to "Welcome"
 }
