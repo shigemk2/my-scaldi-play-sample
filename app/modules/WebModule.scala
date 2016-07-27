86679bc4eaf7c3e2eb87217180ab3fcc19cb5808
shigemk2 <i.am.shige@gmail.com>
2016-07-28 00:09:02 +0900

    Change order

--- app/modules/WebModule.scala
+++ app/modules/WebModule.scala
@@ -5,6 +5,6 @@ import service.{MessageService, OfficialMessageService, SimpleMessageService}
 import scaldi.play.condition._
 
 class WebModule extends Module {
-  bind [MessageService] when (inDevMode or inTestMode) to new SimpleMessageService
   bind [MessageService] when inProdMode to new OfficialMessageService
+  bind [MessageService] when (inDevMode or inTestMode) to new SimpleMessageService
 }
